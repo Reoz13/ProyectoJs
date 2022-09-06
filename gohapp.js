da2fb5a84537456e1917006264e7fc500bdc4680
Uriel Gomez <gomez.uriel.a@gmail.com>
2022-09-05 22:06:15 -0300

    se agregaron estilos a las cards

--- gohapp.js
+++ gohapp.js
@@ -12,40 +12,39 @@ const mostrarExcursiones = (array) => {
   array.forEach((element) => {
     const excursiones_hijo = document.createElement("div");
     excursiones_hijo.classList.add("row", "card2");
-    excursiones_hijo.innerHTML = `<div
-            class="col-md-10 card_left d-flex flex-column justify-content-end border-end"
-          >
+    excursiones_hijo.innerHTML = `<div class="col-md-9 card_left d-flex flex-column justify-content-end border-end ">
             <div class="border-bottom">
-              <h4>${element.nombre}</h4>
+              <h4 class="titulo_act">${element.nombre}</h4>
               <div class="d-flex">
-                <p>Duración: ${element.duracion}.</p>
+                <p class="datos_extra">Duración: ${
+                  element.duracion
+                }. Disponible: ${element.disponible}</p>
               </div>
             </div>
 
             <div class="border-bottom">
-              <h3>Incluye</h3>
-              <span
-                >${element.short_desc}</span
+              <h3 class="incluye_act mt-5">Incluye</h3>
+              <span class="incluye_desc">${element.short_desc}</span
               >
               <!-- aca va esto En este recorrido por el Parque Ischigualasto - Valle de la Luna disfrutarás de paisajes de otro mundo. Ven a recorrer esta maravilla natural que te impresionará por sus formaciones rocosas y colores rojizos. -->
             </div>
 
-            <div class="d-flex gap-3">
+            <div class="d-flex gap-3 mt-5">
               <div>
-                <h3>Fecha</h3>
-                <input type="date" name="date_excursion" id="date" />
+                <h3 class="datos_reserva">Fecha</h3>
+                <input class="redondeado"type="date" name="date_excursion" id="date" />
               </div>
               <div>
-                <h3>Hora</h3>
-                <select name="hora_excursion" id="hora">${element.horas.map(
+                <h3 class="datos_reserva">Hora</h3>
+                <select class ="redondeado"name="hora_excursion" id="hora">${element.horas.map(
                   (f) => `<option value=${f}>${f}</option>`
                 )}</select>
               </div>
               <div>
-                <h3>Cantida de personas</h3>
+                <h3 class="datos_reserva">Cantida de personas</h3>
                 <button
                   type="button"
-                  class="btn btn-lg"
+                  class="btn btn-lg redondeado"
                   data-bs-toggle="popover"
                   data-bs-title="Popover title"
                   data-bs-content="+ personas"
@@ -56,12 +55,13 @@ const mostrarExcursiones = (array) => {
             </div>
           </div>
 
-          <div class="col-md-2 d-flex">
+          <div class="col-md-3 d-flex">
             <div class="precio d-flex flex-column">
-              <h3>Precio final por 1 adulto</h3>
-              <span>$${element.precio} </span>
-              <button>Comprar</button>
-              <button>Agregar</button>
+              <h3 class="m-3 precio">Precio final por 'x' persona</h3>
+              <span class="mt-5">$${element.precio} </span>
+              <button class="redondeado boton p-2 m-2
+              ">Comprar</button>
+              <button class="redondeado boton p-2">Agregar</button>
             </div>
           </div>`;
     excursionesPadre.prepend(excursiones_hijo);
